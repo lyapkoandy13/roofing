0b375c5b97392f2eef77a8da7f6a26cfbdecf1af
Andrew <lyapkoandy13@gmail.com>
2017-02-24 01:54:42 +0200

    some change
    some change

--- roofing/wsgi.py
+++ roofing/wsgi.py
@@ -6,11 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roofing.settings")
+curr_dir = os.path.dirname(__file__)
+
+sys.path.append(curr_dir)
+sys.path.append(os.path.dirname(curr_dir))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 application = get_wsgi_application()
